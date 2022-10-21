1d088154934ee5d0a0a9f5ae15d01ebc2839ab27
adrianaperez123465 <112712237+adrianaperez123465@users.noreply.github.com>
2022-10-20 23:38:41 -0600

    Update Ejercicio1.js

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -2,7 +2,7 @@
 
 
 function first() {
-    let array= [3,1,2,6,4,6,3,9,4,2]
+    let array= [3,1,2,6,4,6,3,9,4,2];
     array.forEach ((valor, índice) => {
         console.log(`Índice: ${[índice]}, valor: ${[valor]}`);
     });
